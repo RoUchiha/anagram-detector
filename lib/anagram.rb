ff2dc087487dc16c543c3188c393ca61049e03ae
RoUchiha <swaggsikh@gmail.com>
2021-06-19 01:29:46 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
-# Your code goes here!
+class Anagram
+
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(anagrams)
+        matches = []
+        jumble = @word.chars.sort.join
+
+        anagrams.each do |a|
+            if a.chars.sort.join == jumble
+                matches << a
+            end
+        end
+        return matches
+
+    end
+
+
+end
